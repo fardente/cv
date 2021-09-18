7b8d01230ba67672b0e4419329e7dc1bf3dcf5d8
Fernando Ardente <ardente@mailbox.org>
2021-09-18 17:07:20 +0200

    First Prototype

--- app.js
+++ app.js
@@ -1,27 +1,82 @@
-const tl = gsap.timeline();
-tl.from("aside", {
-    duration: 1,
-    x: "-100%",
-    stagger: 0.5,
-    ease: "bounce.out",
-})
-    .from(
-        "main",
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.set(".title h1", { zIndex: 400, delay: 0.1 })
+    .to(".title h1", {
+        opacity: 1,
+        duration: 1.5,
+        stagger: 0.4,
+    })
+    .to(".title h1", {
+        y: 0,
+        stagger: 0.2,
+        duration: 0.8,
+    })
+    .to(
+        ".loading",
         {
-            duration: 0.5,
-            opacity: 0,
-            y: "200%",
-            stagger: 0.2,
+            opacity: 1,
+            y: "+=100%",
+            duration: 1,
         },
-        "-=1"
+        "-=0.5"
     )
-    .from(
-        ".title h1",
-        {
-            y: -300,
-            duration: 2,
-            ease: "expo.inOut",
-        },
-        "-=1"
-    )
-    .from(".title h3", { duration: 1, x: "-=200%" }, "-=2");
+    .from("body", {
+        overflow: "hidden",
+    });
+// .fromTo(
+//     ".loading",
+//     {
+//         opacity: 1,
+//     },
+//     {
+//         opacity: 0,
+//         y: "+=80%",
+//         delay: 13,
+//         duration: 5,
+//         ease: "power4",
+//     }
+// );
+
+// tl.from(".container", {
+//     opacity: 0,
+//     delay: 0.5,
+// }).to(".container", {
+//     opacity: 1,
+//     duration: 0.5,
+// });
+// tl.from(".title h1", {
+//     y: 20,
+//     opacity: 0,
+//     delay: 0.5,
+// }).to(".title h1", {
+//     y: 0,
+//     opacity: 1,
+//     duration: 0.5,
+// });
+
+// tl.from("aside", {
+//     duration: 1,
+//     x: "-100%",
+//     stagger: 0.5,
+//     ease: "bounce.out",
+// })
+//     .from(
+//         "main",
+//         {
+//             duration: 0.5,
+//             opacity: 0,
+//             y: "200%",
+//             stagger: 0.2,
+//         },
+//         "-=1"
+//     )
+//     .from(
+//         ".title h1",
+//         {
+//             y: -300,
+//             duration: 2,
+//             ease: "expo.inOut",
+//         },
+//         "-=1"
+//     )
+//     .from(".title h3", { duration: 1, x: "-=200%" }, "-=2");
